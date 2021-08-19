234e6df4ae07c43381f6b8dc2e3f0efe531c83a0
nafirdausa <bless.azz35@gmail.com>
2021-08-19 20:00:50 +0700

    mencari error pada section product di file index.html dengan menyeleksi file men.html, product.css, script.js yang mana diberi komentar yg berkaitan dg indikasi yg membuat error. Penyebab error ialah di file index.html terdapat nama id yang sama seperti di men.html yaitu sama-sama ber id='product'

--- script.js
+++ script.js
@@ -6,79 +6,81 @@ menuToggle.addEventListener('click', function(){
 })
 
 // show product
-const rowProduct = document.getElementById('product');
-var html = '';
+// const rowProduct = document.getElementById('product');
+// var html = '';
 
-var dataProduct = [
-    {
-        "title": "Product A",
-        "price": 100000,
-        "image": "img/portofolio/1.jpg"
-    },
-    {
-        "title": "Product B",
-        "price": 110000,
-        "image": "img/portofolio/2.jpg"
-    },
-    {
-        "title": "Product C",
-        "price": 120000,
-        "image": "img/portofolio/3.jpg"
-    },
-    {
-        "title": "Product D",
-        "price": 130000,
-        "image": "img/portofolio/4.jpg"
-    },
-    {
-        "title": "Product E",
-        "price": 100000,
-        "image": "img/portofolio/1.jpg"
-    },
-    {
-        "title": "Product F",
-        "price": 110000,
-        "image": "img/portofolio/2.jpg"
-    },
-    {
-        "title": "Product G",
-        "price": 120000,
-        "image": "img/portofolio/3.jpg"
-    },
-    {
-        "title": "Product H",
-        "price": 130000,
-        "image": "img/portofolio/4.jpg"
-    },
-    {
-        "title": "Product I",
-        "price": 100000,
-        "image": "img/portofolio/1.jpg"
-    },
-    {
-        "title": "Product J",
-        "price": 110000,
-        "image": "img/portofolio/2.jpg"
-    },
-    {
-        "title": "Product K",
-        "price": 120000,
-        "image": "img/portofolio/3.jpg"
-    },
-    {
-        "title": "Product L",
-        "price": 130000,
-        "image": "img/portofolio/4.jpg"
-    }
-];
+// var dataProduct = [
+//     {
+//         "title": "Product A",
+//         "price": 100000,
+//         "image": "img/portofolio/1.jpg"
+//     },
+//     {
+//         "title": "Product B",
+//         "price": 110000,
+//         "image": "img/portofolio/2.jpg"
+//     },
+//     {
+//         "title": "Product C",
+//         "price": 120000,
+//         "image": "img/portofolio/3.jpg"
+//     },
+//     {
+//         "title": "Product D",
+//         "price": 130000,
+//         "image": "img/portofolio/4.jpg"
+//     },
+//     {
+//         "title": "Product E",
+//         "price": 100000,
+//         "image": "img/portofolio/1.jpg"
+//     },
+//     {
+//         "title": "Product F",
+//         "price": 110000,
+//         "image": "img/portofolio/2.jpg"
+//     },
+//     {
+//         "title": "Product G",
+//         "price": 120000,
+//         "image": "img/portofolio/3.jpg"
+//     },
+//     {
+//         "title": "Product H",
+//         "price": 130000,
+//         "image": "img/portofolio/4.jpg"
+//     },
+//     {
+//         "title": "Product I",
+//         "price": 100000,
+//         "image": "img/portofolio/1.jpg"
+//     },
+//     {
+//         "title": "Product J",
+//         "price": 110000,
+//         "image": "img/portofolio/2.jpg"
+//     },
+//     {
+//         "title": "Product K",
+//         "price": 120000,
+//         "image": "img/portofolio/3.jpg"
+//     },
+//     {
+//         "title": "Product L",
+//         "price": 130000,
+//         "image": "img/portofolio/4.jpg"
+//     }
+// ];
 
-for(product of dataProduct){
-    html += '<div class="col-men-product">'+
-        '<img src='+product.image+' width="350px" height="300px" >'+'<BR>'+
-        'Title : '+product.title+'<BR>'+
-        'Price : '+product.price+'<BR>'+
-    '</div>';
-}
+// for(product of dataProduct){
+//     html += '<div class="col-men-product">'+
+//         '<img src='+product.image+' width="350px" height="300px" >'+'<BR>'+
+//         'Title : '+product.title+'<BR>'+
+//         +product.price+'<BR>'+
+//     '</div>';
+// }
 // console.log(rowProduct);
 
-rowProduct.innerHTML = html;
+// rowProduct.innerHTML = html;
+// const price = querySelector('price');
+// price.style.color = 'red';
